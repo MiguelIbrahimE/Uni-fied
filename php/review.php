4536dc5da4ef2c9b19d7843e9ddf6f0b535e6aa8
MiguelIbrahimE <121685654+MiguelIbrahimE@users.noreply.github.com>
2023-04-27 12:06:35 +0300

    Update review.php
    Hey you had a security issue here, "do not trust inputs"

--- php/review.php
+++ php/review.php
@@ -11,6 +11,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   require_once "../config/cfg.php";
   $name = $_POST["name"];
   $rating = $_POST["rating"];
+  // Check if the rating value is valid
+  if (!is_numeric($rating) || $rating < 1 || $rating > 5) {
+    $response = array("status" => "error", "message" => "Invalid rating value. Please enter a value between 1 and 5.");
+    echo json_encode($response);
+    exit;
+  }
   try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -25,8 +31,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $user_id = $user_row['ID'];
 
       // Check if the user has already rated this restaurant
-      $check_query = "SELECT * FROM rated_restaurants WHERE NAME = '$name' AND USER_ID = '$user_id'";
-      $check_result = $db->query($check_query);
+      $check_query = "SELECT * FROM rated_restaurants WHERE NAME = ? AND USER_ID = ?";
+      $stmt_check = $db->prepare($check_query);
+      $stmt_check->bind_param("si", $name, $user_id);
+      $stmt_check->execute();
+$check_result = $stmt_check->get_result();$check_result = $db->query($check_query);
       if ($check_result->num_rows > 0) {
         $response = array("status" => "error", "message" => "You have already rated this restaurant.");
         echo "<script>alert('You have already rated this restaurant.');</script>";
@@ -38,9 +47,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt = $pdo->prepare("UPDATE restaurants SET Rating = ?, NUMBER_OF_RATINGS = ? WHERE NAME = ?");
         $stmt->execute([$new_rating, $current_number_of_ratings + 1, $name]);
         
-        // Insert the rating into the rated_restaurants table
-        $insert_query = "INSERT INTO rated_restaurants (NAME, RATING, USER_ID) VALUES ('$name', '$rating', '$user_id')";
-        if (!$db->query($insert_query)) {
+        $insert_query = "INSERT INTO rated_restaurants (NAME, RATING, USER_ID) VALUES (?, ?, ?)";
+        $stmt_insert = $db->prepare($insert_query);
+        $stmt_insert->bind_param("sii", $name, $rating, $user_id);
+        if (!$stmt_insert->execute()) {
             die('Failed to insert rating: ' . $db->error);
         } else {
           $response = array("status" => "success", "message" => "Review added successfully.");
